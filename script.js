b9cc3bdec1a40aa5f2982327e9c02eb3697ae897
Jason Felice <jasonfelice0@gmail.com>
2022-04-04 06:48:09 -0700

    Finish the mobile meny ui logic

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.querySelector(".menu-button").addEventListener("click", function() {
     document.querySelector(".mobile-menu").classList.toggle("appear");
     document.querySelector(".menu-button").classList.toggle("cross-button");
 })
+
+for(let i=0;i<document.querySelectorAll(".mobile-menu li a").length;i++) {
+    const selectedElement = document.querySelectorAll(".mobile-menu li a")[i];
+    selectedElement.addEventListener("click", function() {
+        document.querySelector(".mobile-menu").classList.toggle("appear");
+    })
+}
