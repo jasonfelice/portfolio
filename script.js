13ee2a3d595bf44f34f73ed78dc6be26db1146cd
Jason Felice <jasonfelice0@gmail.com>
2022-04-04 11:43:21 -0700

    Fix all errors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.querySelector('.menu-button').addEventListener('click', function () {
-    document.querySelector('.mobile-menu').classList.toggle('appear');
+document.querySelector('.menu-button').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('appear');
   document.querySelector('.menu-button').classList.toggle('cross-button');
-})
+});
 
-for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i++) {
+for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i+=1) {
   const selectedElement = document.querySelectorAll('.mobile-menu li a')[i];
-  selectedElement.addEventListener('click', function () {
+  selectedElement.addEventListener('click', () => {
     document.querySelector('.mobile-menu').classList.toggle('appear');
     document.querySelector('.menu-button').classList.toggle('cross-button');
-    });
+  });
 }
