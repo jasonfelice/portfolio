fadd6299a03cae9d79a39eb44cfd9d193ebbcecc
Jason Felice <jasonfelice0@gmail.com>
2022-04-04 11:45:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector('.menu-button').addEventListener('click', () => {
   document.querySelector('.menu-button').classList.toggle('cross-button');
 });
 
-for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i+=1) {
+for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i += 1) {
   const selectedElement = document.querySelectorAll('.mobile-menu li a')[i];
   selectedElement.addEventListener('click', () => {
     document.querySelector('.mobile-menu').classList.toggle('appear');
