964c673d34fda0055370fbabe982214153a3b8aa
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-05 18:10:57 +0300

    update nav items

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ document.querySelector('.menu-button').addEventListener('click', () => {
   document.querySelector('.mobile-menu').classList.toggle('appear');
   document.querySelector('.menu-button').classList.toggle('cross-button');
 });
+const navItems = document.querySelectorAll('.mobile-menu li a');
 
-for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i += 1) {
-  const selectedElement = document.querySelectorAll('.mobile-menu li a')[i];
-  selectedElement.addEventListener('click', () => {
+// for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i += 1) {
+//   const selectedElement = document.querySelectorAll('.mobile-menu li a')[i];
+//   selectedElement.addEventListener('click', () => {
+//     document.querySelector('.mobile-menu').classList.toggle('appear');
+//     document.querySelector('.menu-button').classList.toggle('cross-button');
+//   });
+// }
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
     document.querySelector('.mobile-menu').classList.toggle('appear');
     document.querySelector('.menu-button').classList.toggle('cross-button');
   });
-}
+});
 
-seeProjectButtons.forEach(function (i){
+seeProjectButtons.forEach(function (i) {
   i.addEventListener('click', () => {
-    document.querySelector('.project-details-modal-background').classList.toggle('disappear');
+    document
+      .querySelector('.project-details-modal-background')
+      .classList.toggle('disappear');
   });
 });
 
 document.querySelector('.dark-cross-button').addEventListener('click', () => {
-  document.querySelector('.project-details-modal-background').classList.toggle('disappear');
+  document
+    .querySelector('.project-details-modal-background')
+    .classList.toggle('disappear');
 });
