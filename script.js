bfe3bdf850b109f4a77f1006ab794a2dd7d3dc0c
Jason Felice <jasonfelice0@gmail.com>
2022-04-05 07:32:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const seeProjectButtons = document.querySelectorAll('.see-project-button');
+
 document.querySelector('.menu-button').addEventListener('click', () => {
   document.querySelector('.mobile-menu').classList.toggle('appear');
   document.querySelector('.menu-button').classList.toggle('cross-button');
@@ -11,12 +13,11 @@ for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i += 
   });
 }
 
-for (let i = 0; i < document.querySelectorAll('.see-project-button').length; i += 1) {
-  const selectedElement = document.querySelectorAll('.see-project-button')[i];
-  selectedElement.addEventListener('click', () => {
+seeProjectButtons.forEach(function (i){
+  i.addEventListener('click', () => {
     document.querySelector('.project-details-modal-background').classList.toggle('disappear');
   });
-}
+});
 
 document.querySelector('.dark-cross-button').addEventListener('click', () => {
   document.querySelector('.project-details-modal-background').classList.toggle('disappear');
