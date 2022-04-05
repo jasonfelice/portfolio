46f7f47669c9f37d52e1b91087246df6a88ff4cd
Jason Felice <jasonfelice0@gmail.com>
2022-04-05 05:14:22 -0700

    Add javaScript to popup project detials modal on click

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ for (let i = 0; i < document.querySelectorAll('.mobile-menu li a').length; i += 
     document.querySelector('.menu-button').classList.toggle('cross-button');
   });
 }
+
+for (let i = 0; i < document.querySelectorAll('.see-project-button').length; i += 1) {
+  const selectedElement = document.querySelectorAll('.see-project-button')[i];
+  selectedElement.addEventListener('click', () => {
+    document.querySelector('.project-details-modal-background').classList.toggle('disappear');
+  });
+}
+
+document.querySelector('.dark-cross-button').addEventListener('click', () => {
+  document.querySelector('.project-details-modal-background').classList.toggle('disappear');
+});
