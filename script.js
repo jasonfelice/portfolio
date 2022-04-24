bc0d4f0ed101e487f8508ab363c80eadf9ce2478
Jason Felice <jasonfelice0@gmail.com>
2022-04-24 03:50:35 -0700

    Fix dynamics bugs: Technologies not parsing when adding cards

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ const formReset = document.querySelector('button[type="reset"]');
 const formInput = { name: '', email: '', message: '' };
 const myPortfolio = [
   {
-    card: { title: 'My Title', description: 'Brief description', techList: ['html', 'bootstrap', 'Ruby'], }
-  }];
+     title: 'My Title',
+     description: 'Brief description',
+     techList: ['html', 'bootstrap', 'Ruby']
+  }
+];
 
 function addCards(i) {
   const cardContainer = document.createElement('div');
@@ -17,16 +20,26 @@ function addCards(i) {
   const cardTitle = document.createElement('h2');
   const cardDescription = document.createElement('p');
   const techContainer = document.createElement('ul');
+  for (let k = 0; k < myPortfolio[i].techList.length; k += 1) {
+    const list = document.createElement('li');
+    list.textContent = myPortfolio[i].techList[k];
+    techContainer.appendChild(list);
+  } 
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.classList.add('see-project-button');
+  button.textContent = 'See Project';
   cardContainer.classList.add('cards');
   cardContent.classList.add('content');
   techContainer.classList.add('other-cards-info');
   techContainer.classList.add('languages');
-  cardTitle.textContent = myPortfolio[i].card.title;
-  cardDescription.textContent = myPortfolio[i].card.description;
+  cardTitle.textContent = myPortfolio[i].title;
+  cardDescription.textContent = myPortfolio[i].description;
   cardContent.appendChild(cardTitle);
   cardContent.appendChild(cardDescription);
   cardContent.appendChild(techContainer);
   cardContainer.appendChild(cardContent);
+  cardContainer.appendChild(button);
   document.querySelector('.work-card-container').appendChild(cardContainer);
 }
 
