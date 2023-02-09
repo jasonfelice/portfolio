6e3aab11a130301c71befa4101b42d61fbe79dde
Jake Felice <jasonfelice0@gmail.com>
2023-02-09 08:32:55 -0800

    Create a new card

--- modules/cardsData.js
+++ modules/cardsData.js
@@ -1,5 +1,15 @@
 const myPortfolio = [
   {
+    id: 1,
+    title: 'Conference Webpage',
+    briefDes: 'It\'s an exact clone of Creative Commons summit webpage.',
+    techList: ['JavaScript', 'html', 'CSS'],
+    image: './images/data/image1.png',
+    detailedDes: 'It is a clone of a creative commons seminar webpage and consists of two pages, both fully responsive for desktop and mobile phones.',
+    liveLink: 'https://jasonfelice.github.io/First-Capstone/',
+    source: 'https://github.com/jasonfelice/First-Capstone/',
+  },
+  {
     id: 2,
     title: 'Conference Webpage',
     briefDes: 'It\'s an exact clone of Creative Commons summit webpage.',
